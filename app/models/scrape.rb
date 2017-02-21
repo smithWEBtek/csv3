2fdac3e842bbf2c5672306e4f94c101289753eb2
smithwebtek <brad@smithwebtek.com>
2017-02-20 20:50:13 -0500

    wrap up for now
    changed ‘w’ (write) to ‘a’ (append) for updating csv file.

--- app/models/scrape.rb
+++ app/models/scrape.rb
@@ -9,42 +9,14 @@ class Scrape < ApplicationRecord
       new_scrape = Scrape.new(company: company_name.text)
       new_scrape.save
     end
-binding.pry
     store_csv(companies)
   end
 
   def self.store_csv(array) 
-    CSV.open('./lib/assets/scrape.csv', 'w') do |csv|
+    CSV.open('./lib/assets/scrape.csv', 'a') do |csv|
       csv << array
     end
   end
 
 end
- 
- # "Boston, MA(857) 259-7717"
-
-# company   parse_page.css('.provider-card__info__header').text  
-# about     parse_page.css('.provider-card__content').first.text
-# contact
-# city      parse_page.css('.provider-card__icon.provider-card__icon__location').first.text
-# state
-
-          # [36] pry(Scrape)> d=_
-          # [37] pry(Scrape)> parse_page.css('.provider-card__icon.provider-card__icon__location').first.text
-          # [37] pry(Scrape)> parse_page.css('.provider-card__icon.provider-card__icon__location').first.text
-          # => "Boston, MA"
-          # [38] pry(Scrape)> z = _
-          # => "Boston, MA"
-          # [39] pry(Scrape)> z.split(",")[1]
-          # => " MA"
-          # [40] pry(Scrape)> z.split(", ")[1]
-          # => "MA"
-          # [41] pry(Scrape)> z.split(", ")[0]
-          # => "Boston"
-
-
-# phone     a = parse_page.css('.provider-card__info__line').split("(")[1]
-#           a = .split("(")[1]
-# website   parse_page.css('provider-card__icon provider-card__icon__website').first.text
-# email
-
+  
