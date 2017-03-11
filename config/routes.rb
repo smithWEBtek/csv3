775cc4d9567880cc623589a12d29080650bdca98
smithwebtek <brad@smithwebtek.com>
2017-03-10 23:30:54 -0500

    forms for company index show new edit, update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
-
-resources :contacts, :jobs, :actions, :companies
+  root 'tools#home'
+  resources :actions, :companies, :contacts, :jobs, :tools
   get '/import_new_companies', to: 'tools#import_new_companies'
   get '/trunc_reset', to: 'tools#trunc_reset'
-  get '/', to: 'tools#home'
-  root to: '/'
 end
